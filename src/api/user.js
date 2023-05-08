27ebf9e9b91c47d4cae6bf77a870d4e4bbf35b45
yooneeee <sjy4489@gmail.com>
2023-05-08 14:07:30 +0900

    [design] 메인페이지 css적용

--- src/api/user.js
+++ src/api/user.js
@@ -3,7 +3,10 @@ import axios from "axios";
 // 로그인
 const login = async (user) => {
   try {
-    const response = await axios.post(`http://3.38.191.164/login`, user);
+    const response = await axios.post(
+      `${process.env.REACT_APP_SERVER_URL}/api/auth/login`,
+      user
+    );
     return response.data;
   } catch (error) {
     console.log(error.response);
@@ -14,11 +17,14 @@ const login = async (user) => {
 // 유저 인증 확인
 const confirm = async (authorization) => {
   try {
-    const response = await axios.get(`http://3.38.191.164/user`, {
-      headers: {
-        authorization: `Bearer ${authorization}`,
-      },
-    });
+    const response = await axios.get(
+      `${process.env.REACT_APP_SERVER_URL}/user`,
+      {
+        headers: {
+          authorization: `Bearer ${authorization}`,
+        },
+      }
+    );
     return response;
   } catch (error) {
     return Promise.reject(error.response);
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,25 +2,69 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-function User({ user }) {
+function User({ user }, user_id) {
   const navigate = useNavigate();
 
-  /*   const handleDetailPageLinkClick = () => {
+  const handleDetailPageLinkClick = () => {
     navigate(`/user/${user_id}`);
-  }; */
+  };
 
   return (
-    <ItemContainer>
-      <h4>{user.username}</h4>
-      <h4> {user.specialty}</h4>
-      <h4>{user.mbti}</h4>
-    </ItemContainer>
+    <UserCard onClick={handleDetailPageLinkClick}>
+      <UserInfo>
+        <img src={user.avatar} width="100px" />
+        <UserName>{user.username}</UserName>
+        <UserSpecialty>주특기 : {user.specialty}</UserSpecialty>
+        <UserMbti>MBTI : {user.mbti}</UserMbti>
+      </UserInfo>
+    </UserCard>
   );
 }
 
 export default User;
 
-const ItemContainer = styled.div`
-  border: solid 1px gray;
-  margin: 20px;
+const UserCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.2);
+  padding: 20px;
+  text-align: center;
+  width: calc(20% - 60px);
+  border-radius: 5px;
+  cursor: pointer;
+  /*   
+  width: calc(20% - 20px);
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
+  text-align: center;
+  h4 {
+    margin: 0;
+  } */
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  /*   margin-top: 20px; */
+`;
+
+const UserName = styled.h4`
+  font-size: 30px;
+  margin-bottom: 5px;
+`;
+
+const UserSpecialty = styled.h4`
+  font-size: 15px;
+  margin-bottom: 5px;
+`;
+
+const UserMbti = styled.h4`
+  font-size: 15px;
+  margin-bottom: 5px;
 `;
